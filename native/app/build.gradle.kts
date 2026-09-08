f0812c8b6a9f7334719ae42ec541ed49bb5af37a
GustBebe <dev@gustbebe.md>
2026-09-08 13:37:40 +0300

    Nativ: scos kotlin.android (incorporat in AGP 9)

--- native/app/build.gradle.kts
+++ native/app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    // Kotlin vine incorporat in AGP 9 (fara org.jetbrains.kotlin.android)
     id("org.jetbrains.kotlin.plugin.compose")
     id("com.google.devtools.ksp")
 }
